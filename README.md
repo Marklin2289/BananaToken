1ae5b918a921d1d6b0f5cad8649f0dc518c4efc6
marklin2289 <marklin2289@gmail.com>
2022-11-07 13:54:13 -0800

    added BananaToken.sol

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Sample Hardhat Project / Token Design
 
-1. initial supply
-2. capped / max supply
+1. initial supply (send to owner) - 70% of capped supply which is 70,000,000
+2. capped / max supply : (capped) - 100,000,000 tokens - 30% of supply are for rewards and features
 3. minting strategy
 4. block reward
 5. burnable
@@ -11,11 +11,11 @@ This project demonstrates a basic Hardhat use case. It comes with a sample contr
 Try running some of the following tasks:
 
 ```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
+yarn hardhat help
+yarn hardhat test
+REPORT_GAS=true yarn hardhat test
+yarn hardhat node
+yarn hardhat run scripts/deploy.js
 ```
 
 
--- contracts/BananaToken.sol
+++ contracts/BananaToken.sol
@@ -4,9 +4,51 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract BananaToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("BananaToken","BNT") {
-        _mint(msg.sender, initialSupply)
+contract BananaToken is ERC20Capped {
+    address payable public owner;
+
+    // we are gonna hardcode the initialSupply to 70,000,000 max-supply is 100,000,000
+    constructor(uint256 cap, uint256 reward)
+        ERC20("BananaToken", "BNT")
+        ERC20Capped(cap * (10**decimals()))
+    {
+        owner = payable(msg.sender);
+        _mint(owner, 70000000 * (10**decimals()));
+        blockReward = reward * (10**decimals());
+    }
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override {
+        if (from != address(0) && to != block.coinbase && block.aoinbase != address(0)) {
+            _mintMinerReward();
+        }
+        super._beforeTokenTransfer(from, to, value);
+    }
+
+    function setBloackReward(uint256 reward) public onlyOwner {
+        blockReward = reward * (10**decimals());
+    }
+
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call thid function");
+        _;
     }
 }
